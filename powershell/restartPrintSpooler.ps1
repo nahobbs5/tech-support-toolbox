f4a35ad0ca80ed1601cb90f6ff9c2fdf8f6e8eae
Nathan A Hobbs <nahobbs5@gmail.com>
2025-07-18 13:03:16 -0400

    Update and rename RestartPrintSpooler.ps1 to restartPrintSpooler.ps1

--- powershell/restartPrintSpooler.ps1
+++ powershell/restartPrintSpooler.ps1
@@ -1,2 +1,4 @@
+#Purpose: Restart the Print Spooler service to resolve common printing issues
+
 Get-Service -Name spooler | Restart-Service
 Write-Host "Print Spooler service restarted successfully."
